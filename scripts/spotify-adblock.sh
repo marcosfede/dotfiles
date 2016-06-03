60ebe27a71b5b4596fc068582c57c9a7877bf93f
Federico Marcos <marcosfede@gmail.com>
2016-06-03 07:53:29 -0300

    spotify scripts changes

--- scripts/spotify-adblock.sh
+++ scripts/spotify-adblock.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 echo "installing new spotify"
-wget http://repository-origin.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.0.31.56.g526cfefe-33_amd64.deb 
-sudo dpkg -i spotify-client_1.0.29.92.g67727800-21_amd64.deb
-sudo rm spotify-client_1.0.29.92.g67727800-21_amd64.deb
+wget -q -O tmp.html http://repository-origin.spotify.com/pool/non-free/s/spotify-client/
+RELEASE_URL=`python .scripts/latestspotify.py`
+rm tmp.html
+wget -q $RELEASE_URL
+
+sudo dpkg -i spotify-client*.deb
+sudo rm spotify-client*.deb
 #########
 echo "spotify addkiller"
 sudo apt-get install -y x11-utils pulseaudio-utils libnotify-bin xdotool vlc
@@ -11,6 +15,8 @@ cd Spotify-AdKiller
 ./install.sh
 cd ..
 sudo rm -R Spotify-AdKiller
-echo "export PATH=$PATH:$HOME/bin" >> ~/.bashrc
-cd ~/bin && mkdir -p dns-block && cd dns-block && wget http://ur1.ca/jpttl -O dns-block.c && gcc -Wall -fPIC -c -o dns-block.o dns-block.c && gcc -shared -fPIC -Wl,-soname -Wl,dns-block.so -ldl -o dns-block.so dns-block.o && cd Downloads
+echo "export PATH=$PATH:$HOME/bin" >> ~/.envvariables
+CURR_DIR=$(pwd -P)
+cd ~/bin && mkdir -p dns-block && cd dns-block && wget http://ur1.ca/jpttl -O dns-block.c && gcc -Wall -fPIC -c -o dns-block.o dns-block.c && gcc -shared -fPIC -Wl,-soname -Wl,dns-block.so -ldl -o dns-block.so dns-block.o
+cd $CURR_DIR
 #########
