fd1f647508402996c1603331ad0d94c98323691d
Federico Marcos <marcosfede@gmail.com>
2016-06-19 02:04:08 -0300

    changed nvim and fish config files

--- files/init.fish
+++ files/init.fish
@@ -1,12 +1,15 @@
-set fish_color_autosuggestion fsbd2f  #suggestions like full directory
-set fish_color_command fe8019  # succesful commands
-# set fish_color_comment green  # comments
-# set fish_color_cwd green  # 
-set fish_color_error fb4934 # errors
-set fish_color_normal ebdbb2 # ??
-set fish_color_param b8bb26 # parameters like directories
-set fish_color_redirection 097ed3 # redirection like > stdin
-set fish_color_operator 097ed3 # operators like |
-set fish_color_search_match --background=665c54 # tab selection 
+set fish_color_search_match --background=665c54
+set fish_color_valid_path fabd2f --underline
+set fish_color_comment cc241d
+set fish_color_param b8bb26
+set fish_color_history_current 83a598
+set fish_color_command 8ec07c
 set fish_color_selection --background=665c54
-
+set fish_color_cwd 8ec07c
+set fish_color_status fb4934
+set fish_color_match d3869b
+set fish_color_cwd_root fb4934
+set fish_color_operator 83a598
+set fish_color_host 83a598
+set fish_color_escape 83a598
+set fish_color_error fb4934
