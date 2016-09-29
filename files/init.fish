bcc15b186919a4f4a99371475429ae9dbfbbc56c
Federico Marcos <marcosfede@gmail.com>
2016-09-29 01:18:33 -0300

    changed .envvariables, .aliases to be bash compatible and  fish through bass, added fishsetup and brew setup

--- files/init.fish
+++ files/init.fish
@@ -10,8 +10,8 @@ set fish_color_operator 097ed3 # operators like |
 set fish_color_search_match --background=665c54 # tab selection 
 set fish_color_selection --background=665c54
 set fish_greeting ""
-. ~/.aliases
-. ~/.envvariables
+bass . ~/.aliases
+bass . ~/.envvariables
 
 function sudo
     if test "$argv" = !!
