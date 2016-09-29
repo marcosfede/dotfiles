232438a68f6e0a0de522d837c2ea19b79e50f335
Federico Marcos <marcosfede@gmail.com>
2016-09-29 02:10:44 -0300

    bass does not work with aliases. added a file .aliasesfish just to handle fish aliases and it gets sourced from the omf init.fish

--- scripts/install_zsh_ohmyzsh.sh
+++ scripts/install_zsh_ohmyzsh.sh
@@ -9,7 +9,7 @@ fi
 if [[ $platform == 'linux' ]]; then
 	sudo apt-get install -y zsh git
 elif [[ $platform == 'mac' ]]; then
-	brew install zsh
+	brew install neovim/neovim/neovim
 fi
 wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O zsh.sh
 sed -i 's/env zsh/#env zsh/g' zsh.sh
