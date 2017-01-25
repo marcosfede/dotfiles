833fe1931624a6d7a67c3cd593e9831add954f11
Federico Marcos <marcosfede@gmail.com>
2017-01-25 02:17:33 -0300

    vim plug changed config dir for neovim??

--- scripts/vimconfig.sh
+++ scripts/vimconfig.sh
@@ -21,3 +21,5 @@ curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
