d281ffc7bc4baef6bc7a08d9546784c68850efd3
Federico Marcos <marcosfede@gmail.com>
2017-01-25 02:00:13 -0300

    added docker script for ubuntu xenial

---
+++ scripts/linux/ubuntu/docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt-get install apt-transport-https ca-certificates
+curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+sudo add-apt-repository \
+       "deb https://apt.dockerproject.org/repo/ \
+       ubuntu-xenial \
+       main"
+sudo apt-get update
+sudo apt-get -y install docker-engine
+sudo groupadd docker
+sudo gpasswd -a ${USER} docker
+sudo service docker restart
+sudo curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/machine/releases/download/v0.9.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
+  sudo chmod +x /tmp/docker-machine &&
+  sudo cp /tmp/docker-machine /usr/local/bin/docker-machinenewgrp
+newgrp docker
