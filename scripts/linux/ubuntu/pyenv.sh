c44ee9cd10db5169c1f5c5ca580f7291a1c09c5a
Federico Marcos <federico.marcos@celerative.com>
2017-03-01 13:32:25 -0300

    pixel scripts and script fixes

--- scripts/linux/ubuntu/pyenv.sh
+++ scripts/linux/ubuntu/pyenv.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
