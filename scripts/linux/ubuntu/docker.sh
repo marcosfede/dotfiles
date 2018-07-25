22b68ae65c0e6ce5c7bbc4b906f727a541cf3a39
Federico Marcos <federico.marcos@celerative.com>
2018-07-25 01:09:55 -0300

    updated docker. updated node. added la-capitaine

--- scripts/linux/ubuntu/docker.sh
+++ scripts/linux/ubuntu/docker.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-sudo apt-get install apt-transport-https ca-certificates
-curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+sudo apt-get install apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
-       "deb https://apt.dockerproject.org/repo/ \
-       ubuntu-xenial \
-       main"
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 sudo apt-get update
 sudo apt-get -y install docker-ce jq
 sudo groupadd docker
---
+++ scripts/linux/ubuntu/la-capitaine-icons.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo add-apt-repository ppa:dyatlov-igor/la-capitaine
+sudo apt update
+sudo apt install la-capitaine-icon-theme
