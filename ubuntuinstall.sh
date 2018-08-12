9123de6504be79267e59355c5dabd03e543bbc1e
Federico Marcos <federico.marcos@celerative.com>
2018-08-12 17:01:41 -0300

    ubuntu script

---
+++ ubuntuinstall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd scripts
+bash fede-git-config.sh
+bash vimconfig.sh
+bash sshkeys.ssh
+bash prompts.sh
+cd linux/ubuntu
+bash archiveformats.sh
+bash firacode.sh
+bash la-capitaine-icons.sh
+bash node.sh
+bash powerlinefonts.sh
+./swap 4G
+bash swappiness.sh
+bash vscode.sh
+bash docker.sh
